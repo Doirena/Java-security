14e7c8cad2d158ce3b723ac276a5c8310d7ce1ad
Doreina <barkauskaite.dovile@gmail.com>
2020-08-05 01:52:42 +0300

    two controller one for-> login and home page and other for registration

---
+++ src/main/java/com/dovile/securityjavalearn/controllers/MainController.java
@@ -0,0 +1,26 @@
+package com.dovile.securityjavalearn.controllers;
+
+import com.dovile.securityjavalearn.entities.User;
+import com.dovile.securityjavalearn.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+    @Autowired
+    private UserService userService;
+    
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/dovile/securityjavalearn/controllers/UserRegistrationController.java
@@ -0,0 +1,38 @@
+package com.dovile.securityjavalearn.controllers;
+
+import com.dovile.securityjavalearn.domain.UserRequest;
+import com.dovile.securityjavalearn.service.UserService;
+import com.dovile.securityjavalearn.validator.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Controller
+@RequestMapping("/registration")
+public class UserRegistrationController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserValidator userValidator;
+
+    @GetMapping
+    public String showRegistrationForm(Model modelMap) {
+        modelMap.addAttribute("user", new UserRequest());
+        return "registration";
+    }
+
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") UserRequest userRequest ) {
+
+        if (userRequest.getEmail().equals("test1@test.com")) {
+            return "registration";
+        }
+        userService.save(userRequest);
+        return "redirect:/registration?success";
+    }
+}
