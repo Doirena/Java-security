9c14d237595fe092816dcad9a1275245bb25c600
Doreina <barkauskaite.dovile@gmail.com>
2020-08-05 01:55:40 +0300

    create UserRepository and add UserRequest class which use for body when registration user

---
+++ src/main/java/com/dovile/securityjavalearn/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.dovile.securityjavalearn.repositories;
+
+import com.dovile.securityjavalearn.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
