3510204f1f6b9d5fbace783115783dac64a1e662
Doreina <barkauskaite.dovile@gmail.com>
2020-08-04 20:10:54 +0300

    Create new java project with security dependency

---
+++ src/main/java/com/dovile/securityjavalearn/SecurityJavaLearnApplication.java
@@ -0,0 +1,13 @@
+package com.dovile.securityjavalearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJavaLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJavaLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dovile/securityjavalearn/SecurityJavaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dovile.securityjavalearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJavaLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
