46b635223b2382cb4cf00fe8de7f9af7e57ccaa7
Doreina <barkauskaite.dovile@gmail.com>
2020-08-13 14:12:36 +0300

    add new method if email exist, add comments

--- src/main/java/com/dovile/securityjavalearn/service/UserServiceImpl.java
+++ src/main/java/com/dovile/securityjavalearn/service/UserServiceImpl.java
@@ -1,12 +1,10 @@
 package com.dovile.securityjavalearn.service;
 
-
 import com.dovile.securityjavalearn.domain.UserRequest;
 import com.dovile.securityjavalearn.entities.Role;
 import com.dovile.securityjavalearn.entities.User;
 import com.dovile.securityjavalearn.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,13 +12,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * @author barkauskaite.dovile@gmail.com
+ */
 @Service
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
@@ -33,24 +31,39 @@ public class UserServiceImpl implements UserService {
         this.userRepository = userRepository;
     }
 
+    /**
+     *
+     * @param userRequest
+     * @return new User
+     */
     @Override
     public User save(UserRequest userRequest) {
         User user = new User(userRequest.getFirstName(),
                 userRequest.getLastName(), userRequest.getEmail(),
                 passwordEncoder.encode(userRequest.getPassword()), Arrays.asList(new Role("ROLE_USER")));
-
-        for (User u : userRepository.findAll()) {
-            if (userRequest.getEmail().equals(u.getEmail())) {
-                //throw new Exception("Someone already has that username.");
-                // throw new DuplicateKeyException("Someone already has that username.");
-                throw new IllegalArgumentException("Someone already has that username.");
-            }
-        }
-
         return userRepository.save(user);
     }
 
+    /**
+     *
+     * @param email
+     * @return true if email exist
+     */
+    @Override
+    public Boolean existByEmail(String email) {
+        User user_email = userRepository.findByEmail(email);
+        if (user_email != null) {
+            return true;
+        }
+        return false;
+    }
 
+    /**
+     *
+     * @param username
+     * @return login user
+     * @throws UsernameNotFoundException
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(username);
@@ -59,6 +72,11 @@ public class UserServiceImpl implements UserService {
         }
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
     }
+    /**
+     *
+     * @param roles
+     * @return role if it is and check which
+     */
     private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
         return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
